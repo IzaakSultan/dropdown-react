8540dcde45f499984baacb082c364476d54e3110
Izaak Sultan <hello@izaaksultan.com>
2016-05-08 00:05:03 +0800

    Add additional dropdown components and bump node-sass version
    Added DropdownLink, DropdownDivider, DropdownCheckbox components.
    Bumped the node-sass dev dependency version to 3.7.0

--- examples/examples.js
+++ examples/examples.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Dropdown, {DropdownItem} from '../lib';
+import Dropdown, {DropdownDivider, DropdownCheckbox, DropdownItem, DropdownLink} from '../lib';
 
 import '../lib/scss/index.scss';
 
@@ -10,6 +10,11 @@ function Example() {
         <div>
             <Dropdown label="Hello World">
                 <DropdownItem>Dropdown Item!</DropdownItem>
+                <DropdownItem>Dropdown Item 2!</DropdownItem>
+                <DropdownItem>Dropdown Item 3!</DropdownItem>
+                <DropdownDivider />
+                <DropdownLink href="http://google.com">I am a link!</DropdownLink>
+                <DropdownCheckbox>Yes</DropdownCheckbox>
             </Dropdown>
         </div>
     );
