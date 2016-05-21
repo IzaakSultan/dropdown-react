6b9825c02946c8d3ba2c2101741a71395e8ab924
Izaak Sultan <izaaksultan@gmail.com>
2016-05-21 22:40:53 +0800

    Force the dropdown portal to be rerendered when its children update

--- examples/examples.js
+++ examples/examples.js
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom';
 
 import Dropdown, {DropdownDivider, DropdownCheckbox, DropdownItem, DropdownLink} from '../lib';
 
-function Example() {
-    return (
-        <div>
-            <Dropdown label="Click Me!">
-                <DropdownItem>Dropdown Item!</DropdownItem>
-                <DropdownDivider />
-                <DropdownLink href="http://google.com">I am a link!</DropdownLink>
-                <DropdownDivider />
-                <DropdownCheckbox>Check me</DropdownCheckbox>
-            </Dropdown>
-        </div>
-    );
+class Example extends React.Component {
+    state = {
+        checked: false
+    };
+
+    render() {
+        return (
+            <div>
+                <Dropdown label="Click Me!">
+                    <DropdownItem>Dropdown Item!</DropdownItem>
+                    <DropdownDivider />
+                    <DropdownLink href="http://google.com">I am a link!</DropdownLink>
+                    <DropdownDivider />
+                    <DropdownCheckbox checked={this.state.checked} onChange={checked => this.setState({checked})}>Check me</DropdownCheckbox>
+                </Dropdown>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<Example />, document.getElementById('root'));
